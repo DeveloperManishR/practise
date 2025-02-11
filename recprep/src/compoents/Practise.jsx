0cfe6d41e1f224961d447f28aa89db50e70cafd8
DeveloperManishR <manishrawat0711@gmail.com>
2025-02-11 13:49:10 +0530

    tailwind

--- recprep/src/compoents/Practise.jsx
+++ recprep/src/compoents/Practise.jsx
@@ -4,12 +4,15 @@ import { useContext } from 'react'
 
 const Practise = () => {
 
-  const values=useContext(CounterContext)
+  const values = useContext(CounterContext)
 
-  console.log("CounterContext",values)
+  console.log("CounterContext", values)
   return (
     <div>
-      <h1 onClick={()=>values.setcount(values.count+1)}>{values.count}</h1>
+      <h1 onClick={() => values.setcount(values.count + 1)}>Click Here {values.count}</h1>
+
+      <button onClick={() => values.setloading
+        (true)} class="text-3xl">Loading</button>
     </div>
   )
 }
--- recprep/src/context/Counter.jsx
+++ recprep/src/context/Counter.jsx
@@ -1,4 +1,4 @@
-import { useState ,createContext} from "react";
+import { useState, createContext } from "react";
 
 
 export const CounterContext = createContext(null)
@@ -6,8 +6,19 @@ export const CounterContext = createContext(null)
 export const Counterprovider = (props) => {
 
     const [count, setcount] = useState(0)
+
+    const [loading, setloading] = useState(false)
     return (
-        <CounterContext.Provider value={{ name: "manish",count ,setcount}}>
+        <CounterContext.Provider value={{ name: "manish", count, setcount, setloading }}>
+            {loading && (
+                <div className="relative z-10">
+                    <div className="fixed inset-0 bg-gray-500/75 transition-opacity data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in">
+                        <div className="flex justify-center items-center w-full h-full">
+                            <div className="w-16 h-16 border-4 border-t-4 border-gray-300 rounded-full animate-spin"></div>
+                        </div>
+                    </div>
+                </div>
+            )}
             {props.children}
         </CounterContext.Provider>
     )
